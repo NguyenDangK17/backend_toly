47f39e2a01480f0c3626c09090476f6f95a9bc1c
DANG NGUYEN <noplstks@gmail.com>
2024-07-16 13:25:45 +0700

    Fix backend

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,11 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://toly.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
@@ -107,7 +111,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://toly.vercel.app",
     // credentials: true,
   },
 });
